ea6924e3d39689b6ea66920401feea129c446f30
GcvKishore <gcvkishore@msitprogram.net>
2021-10-25 19:36:34 +0530

    Code for dataframes.

--- learning_pandas.py
+++ learning_pandas.py
@@ -14,4 +14,27 @@ d = { "name" : [ "Ellie", "Gayatri", "Rebecca", "Rishab" ],
 df = pd.DataFrame(d)
 print(df)
 
-# pd.DataFrame
+# You can also make a new dataframe by loading a pre-existing CSV
+icecream_df = pd.read_csv("All-IceCream.csv")
+print(icecream_df)
+print(icecream_df.head())
+
+# Once you've created a dataframe, you can access the columns directly using indexing with the column name
+print(df["name"])
+print(df["major"])
+
+# You can also add a new column, by setting the index to a new list. The list must be the same length as the existing data.
+
+df["ice cream"] = [ "peach", "chocolate", "caramel", "rocky road" ]
+print(df)
+
+# If you want to access an individual piece of data, you need to iterate over the dataframe,
+# accessing each row one at a time.
+
+# iterrows iterates over both the numerical indexes and the row lists of the dataframe. It's like for-range and for-each combined.
+for index, row in df.iterrows():
+    print(index)
+    print(row)
+    # Then you can index into the row based on column names.
+    print("Name:", row["name"])
+    print() 
